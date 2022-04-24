8fe8715e2a756a8ae5c7178858038637b480a9ad
Mohamed-84Taher <benarbiamohamed84@gmail.com>
2022-04-24 09:11:50 +0100

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+// connect server to database
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`mongodb connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error:${error}`);
+    process.exit(1); // exit with failure
+  }
+};
+
+export default connectDB;
